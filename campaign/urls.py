4274a979decd48867f25a8b6a709c4e1719e963c
prajwal.jumde <prajwal.jumdeu@aminfoweb.co.in>
2024-10-02 05:46:43 +0530

    call logs page from mongo db with recording and transcript implemented

--- campaign/urls.py
+++ campaign/urls.py
@@ -9,6 +9,13 @@ urlpatterns = [
         path('campaign/<int:campaign_id>/contacts/', views.contact_list, name='contact_list'),      
         path('campaign/<int:campaign_id>/contacts/delete', views.delete_contact, name='delete_contacts'),
         path('/start_campaign', views.start_campaign, name='start_campaign'),
-        path('/call_logs', views.list_call_logs, name='list_call_logs')
+        path('/call_logs', views.list_call_logs, name='list_call_logs'),
+        path('/call_details', views.fetch_call_details, name='fetch_call_details'),
+        path('/fetch_audio', views.fetch_audio, name='fetch_audio'),
+        path('/edit_summary', views.edit_summary, name='edit_summary'),
+        path('/edit_transcript', views.edit_transcript, name='edit_transcript'),
+        
+        
+        
         
 ]
