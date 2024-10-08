607cca23ffdbe45f1861379af1a6d2e4f7660781
deep raj <deep.raj.aminfoweb.co.in>
2024-10-08 20:52:27 +0530

    add dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,5 @@
 from django.urls import path,include
 from .views import calls_per_hour
 urlpatterns = [
-    path('', calls_per_hour, name='calls_per_hour'),
+    path('', calls_per_hour, name='dashboard'),
 ]
