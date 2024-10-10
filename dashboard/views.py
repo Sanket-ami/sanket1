24b03df4970849e8fd458e076f1eec28b0ac3cb1
deep raj <deep.raj.aminfoweb.co.in>
2024-10-10 21:00:18 +0530

    changed dashboard view.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -23,6 +23,7 @@ def update_interval_count(intervals, durations):
     return intervals
 
 def calls_per_hour(request):
+    print(datetime.now())
     if request.user.is_superuser:
         organisation_list = User.objects.values('organisation_name').distinct('organisation_name')
     else:
@@ -39,101 +40,50 @@ def calls_per_hour(request):
         if event:
             org = request.GET.get('org')
             campaign_ids = Campaign.objects.filter(organisation_name=org).values_list('id', flat=True)
-            #data = CallLogs.objects.filter(created_at__lt=start_time, created_at__gte=end_time, campaign_id__in=campaign_ids)
-
-            today = datetime.today()
+            today = datetime.today() + timedelta(hours=5, minutes=30)
             total_dur_list = []
-            connected_call_list = []
-            total_call_list = []
             failedCalls = 0
             avg_handling_time = []
             dates = []
             number_of_calls = {'< 5': 0, '5-10': 0, '10-20': 0, '20-30': 0, '30-40': 0, '40-50': 0, '> 50':0}
-            if event == 'daily':
-               for date in range(1):  # Assuming 'monthly' as the default event type
-                    start_time = datetime.combine(today.date() - timedelta(days=date), time.min)
-                    end_time = datetime.combine(today.date()-timedelta(days=date+1), time.min)
+            event_date_range = {'daily': 1, 'weekly': 7, 'monthly': 30}
+            date_range = event_date_range[request.GET.get('event')]
+            connected_call = 0
 
-                    data = CallLogs.objects.filter(created_at__lt=start_time, created_at__gte=end_time, campaign_id__in=campaign_ids)
-                    total_dur = 0
-                    connected_call = 0
-                    total_call = data.count() 
-                    results = []
-                    for call in data:
-                        results.append({'created_at': call.created_at})
-                        if call.call_status not in ['ongoing', 'completed']:
-                            continue
-                        
-                        if hasattr(call, 'call_duration'):
-                            if call.call_duration < 10:
-                                failedCalls += 1
-                                pass
-                            total_dur += call.call_duration
-                            connected_call += 1
-                            total_dur_list.append(call.call_duration)
-                    dates.append(end_time.strftime("%d"))
-                    avg_handling_time.append((total_dur / 60) / connected_call if connected_call > 0 else 0)
-            elif event == 'weekly':
-                for date in range(7):  # Assuming 'monthly' as the default event type
-                    start_time = datetime.combine(today.date() - timedelta(days=date), time.min)
-                    end_time = datetime.combine(today.date()-timedelta(days=date+1), time.min)
+            for date in range(date_range):  # Assuming 'monthly' as the default event type
+                start_time = datetime.combine(today.date() - timedelta(days=date), time.min)
+                end_time = datetime.combine(today.date()-timedelta(days=date+1), time.min)
+                print(start_time, end_time)
+                data = CallLogs.objects.filter(created_at__lte=start_time, created_at__gte=end_time, campaign_id__in=campaign_ids)
+                print("data  =======>", len(data))
+                total_dur = 0
+                total_call = data.count() 
+                results = []
+                for call in data:
+                    results.append({'created_at': call.created_at})
+                    if call.call_status not in ['ongoing', 'completed']:
+                        print(call)
+                        continue
+                    if hasattr(call, 'call_duration'):
+                        connected_call += 1
+                        if call.call_duration < 10:
+                            failedCalls += 1
+                            pass
+                        total_dur += call.call_duration
+                        total_dur_list.append(int(call.call_duration / 60))
 
-                    data = CallLogs.objects.filter(created_at__lt=start_time, created_at__gte=end_time, campaign_id__in=campaign_ids)
-                    total_dur = 0
-                    connected_call = 0
-                    total_call = data.count() 
-                    results = []
-                    for call in data:
-                        results.append({'created_at': call.created_at})
-                        if call.call_status not in ['ongoing', 'completed']:
-                            continue
-                        
-                        if hasattr(call, 'call_duration'):
-                            if call.call_duration < 10:
-                                failedCalls += 1
-                                pass
-                            total_dur += call.call_duration
-                            connected_call += 1
-                            total_dur_list.append(call.call_duration)
-
-                    dates.append(end_time.strftime("%d"))
-                    avg_handling_time.append(int(total_dur / 60) / connected_call if connected_call > 0 else 0)
-
-            else:
-                for date in range(30):  # Assuming 'monthly' as the default event type
-                    start_time = datetime.combine(today.date() - timedelta(days=date), time.min)
-                    end_time = datetime.combine(today.date()-timedelta(days=date+1), time.min)
-                    data = CallLogs.objects.filter(created_at__lt=start_time, created_at__gte=end_time, campaign_id__in=campaign_ids)
-                    total_dur = 0
-                    connected_call = 0
-                    total_call = data.count() 
-                    results = []
-                    for call in data:
-                        results.append({'created_at': call.created_at})
-                        if call.call_status not in ['ongoing', 'completed']:
-                            continue
-                        
-                        if hasattr(call, 'call_duration'):
-                            if call.call_duration < 10:
-                                failedCalls += 1
-                                pass
-                            total_dur += call.call_duration
-                            connected_call += 1
-                            total_dur_list.append(call.call_duration)
-
-                    dates.append(end_time.strftime("%d"))
-                    avg_handling_time.append((total_dur / 60) / connected_call if connected_call > 0 else 0)
+                dates.append(end_time.strftime("%d"))
+                avg_handling_time.append(int(total_dur / 60) / connected_call if connected_call > 0 else 0)
 
             interval = update_interval_count(number_of_calls, total_dur_list)
-            number_of_calls, number_of_calls_labels = list(interval.values()), list(interval.keys())   #total_calls, failed_calls, call_overview
+            number_of_calls, number_of_calls_labels = list(interval.values()), list(interval.keys())
+
             if request.user.is_superuser:
                 organisation_list = User.objects.values('organisation_name').distinct('organisation_name')
             else:
                 organisation_list = [{'organisation_name': request.user.organisation_name}]
             response_data ={
                 'results': results,
-                'start_date': start_time,
-                'end_date': end_time,
                 'avg_handling_time': avg_handling_time,
                 'total_call': total_call,
                 'connected_call': connected_call,
@@ -144,11 +94,9 @@ def calls_per_hour(request):
                 'number_of_calls_labels': number_of_calls_labels,
                 'call_overview':['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
                 'failed_calls': [7, 2, 6, 4,5, 6, 7],
-                'failed_call': 10,
-                "compromised_call": 30,
-                'totalDialedCalls': len(data),
-                'compromisedCalls':len(data) - failedCalls,
-                'failedCalls': failedCalls, 
+                'totalDialedCalls': connected_call,
+                'compromised_call':connected_call - failedCalls,
+                'failed_call': failedCalls, 
             }
 
             return JsonResponse(response_data)  
